515d7f6b9e146de22e916a33350a2d2165fde0bb
HeartBeat <110215026+Heartestrella@users.noreply.github.com>
2024-06-02 18:55:41 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import re
 import sys
 import time
 
-from tools import login, scrape,Movies,login163
+from tools import Movies, login, login163, scrape
 
 
 def load_config():
@@ -30,9 +30,9 @@ def load_config():
             "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36 Edg/116.0.1938.81",
         },
         "cookies": {},
-        "163_coolies" : {},
-        "use_webdriver" : True,
-        "multithreading" : False,
+        "163_coolies": {},
+        "use_webdriver": True,
+        "multithreading": False,
     }
     config_file = "config.json"
 
@@ -46,6 +46,7 @@ def load_config():
             print("配置文件初始化完毕，请手动修改某些数值")
             sys.exit()
 
+
 if __name__ == "__main__":
     config = load_config()
     if not config["cookies"]:
@@ -54,28 +55,27 @@ if __name__ == "__main__":
         config["cookies"] = cookies
         with open("config.json", "w") as f:
             json.dump(config, f, indent=4)
-    
-    if not config["163_coolies"]:
-        cookies = login163.CloudMusicLogin(config)
-        session = cookies.login()
-        cookies.islogin(session)
-        
-    # movies = scrape.BilibiliMovieScraper("https://www.bilibili.com/movie/", config)
-    # movies = movies.get_moive()
-    # Downloader_ = Movies.Downloader_movie(config, config["cookies"])
-    # sleep_time = config["sleep_time"]
-    # for movie in movies:
-    #     for name, values in movie.items():
-    #         match = re.search(r"/ep(\d+)", values[0])
-    #         if match:
-    #             target = match.group(1)
-    #             if Downloader_.get_epid_video(target, name):
-    #                 print(
-    #                     f"电影 {name} 下载完成! 休眠：{sleep_time} 后继续下载下一部电影"
-    #                 )
-    #                 time.sleep(sleep_time)
-    #             else:
-    #                 print(f"电影")
-    #         else:
-    #             print("没匹配到id")
 
+    # if not config["163_coolies"]:
+    #     cookies = login163.CloudMusicLogin(config)
+    #     session = cookies.login()
+    #     cookies.islogin(session)
+
+    movies = scrape.BilibiliMovieScraper("https://www.bilibili.com/movie/", config)
+    movies = movies.get_moive()
+    Downloader_ = Movies.Downloader_movie(config, config["cookies"])
+    sleep_time = config["sleep_time"]
+    for movie in movies:
+        for name, values in movie.items():
+            match = re.search(r"/ep(\d+)", values[0])
+            if match:
+                target = match.group(1)
+                if Downloader_.get_epid_video(target, name):
+                    print(
+                        f"电影 {name} 下载完成! 休眠：{sleep_time} 后继续下载下一部电影"
+                    )
+                    time.sleep(sleep_time)
+                else:
+                    print(f"电影")
+            else:
+                print("没匹配到id")
